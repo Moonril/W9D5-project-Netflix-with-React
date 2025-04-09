21f129cc302d90c6b4a421ed3502b6cc66734401
Vincenza <vinni2690@hotmail.com>
2025-04-09 17:41:32 +0200

    comments bit added

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -3,7 +3,7 @@ import { Link, useParams, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
 const URL = 'https://www.omdbapi.com/?apikey=469ff3dc&i='
-const URLComments = 'https://striveschool-api.herokuapp.com/api/comments/:'
+const URLComments = 'https://striveschool-api.herokuapp.com/api/comments/'
 const APIKey = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2VlN2QzNTFkNDM2ZTAwMTVkYTI3MTkiLCJpYXQiOjE3NDM2ODI4NjksImV4cCI6MTc0NDg5MjQ2OX0.antMXaShZo-QOGkmltLTzqOhABGF7TFwjaHPULTikjI"
 
 const MovieDetails = function () {
@@ -13,7 +13,7 @@ const MovieDetails = function () {
     
 
     const [movie, setMovie] = useState({})
-    const [comments, setComments] = useState({})
+    const [comments, setComments] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [isError, setIsError] = useState(false)
 
@@ -66,7 +66,7 @@ const MovieDetails = function () {
             }
         })
         .then((data) => {
-            console.log('data', data)
+            console.log('getcomments', data)
     
             setComments(data)
             setIsLoading(false)
@@ -83,6 +83,7 @@ const MovieDetails = function () {
 
     useEffect(()=>{
         getMovie()
+        getComments()
     },[])
 
 
@@ -110,14 +111,26 @@ const MovieDetails = function () {
             }
 
             <Row className='justify-content-center'>
-                <Col key={movie.imdbID} xs={12} className="px-0 mb-1 mb-md-0 mx-sm-0">
+                <Col key={movie.imdbID} xs={6} className="px-0 mb-1 mb-md-0 mx-sm-0">
                     <Card className="border-0 p-1 bg-dark" >
-                        <Card.Img variant="top" src={movie.Poster} alt={movie.Title} className="w-100 locandine"/>
-                        <Card.Title className="flex-grow-1 fs-1 text-light pt-3">{movie.Title} <span className='fs-4 text-secondary'>{movie.Runtime}</span><span className='fs-4 text-secondary'> - {movie.Genre}</span></Card.Title>
-                        <Card.Text className='text-light pt-3 text-center'>{movie.Plot}</Card.Text>
-                        <Card.Text className='text-light pt-3 text-center'>Writer: {movie.Writer}</Card.Text>
-                        <Card.Text className='text-light pt-3 text-center'>Director: {movie.Director}</Card.Text>
+                        <Card.Img variant="top" src={movie.Poster} alt={movie.Title} className="w-100 locandina"/>
+                        <Card.Title className="fs-1 text-light pt-3 d-flex justify-content-between">{movie.Title} <span className='fs-4 text-secondary'>{movie.Runtime} - {movie.Genre}</span></Card.Title>
+                        <Card.Text className='text-light text-center pt-3'>{movie.Plot}</Card.Text>
+                        <Card.Text className='text-light pt-2 m-0'>
+                            Writer: {movie.Writer}</Card.Text>
+                        <Card.Text className='text-light'>Director: {movie.Director}</Card.Text>
                     </Card>
+                    <div className='text-light text-center d-flex flex-column justify-content-center border border-1'>
+
+                    {
+                        comments.map((comment)=>(
+                            <div key={comment._id}>
+                                <div>{comment.comment} - {comment.rate}/5</div>
+                            </div>
+
+                    ))
+                    }
+                    </div>
                 </Col>
             </Row>
         </Container>
